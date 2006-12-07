dee26116374f828777ec7b0f87320ea67ccec35b
pinky <>
2006-12-07 13:47:21 +0000

    Adding graphs for new view_mode

---
+++ bin/widget/view/graph_gtk/parser.py
@@ -0,0 +1,53 @@
+##############################################################################
+#
+# Copyright (c) 2004-2006 TINY SPRL. (http://tiny.be) All Rights Reserved.
+#
+# $Id: parser.py 4698 2006-11-27 12:30:44Z ced $
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+##############################################################################
+
+import locale
+import gtk
+from gtk import glade
+
+import tools
+from rpc import RPCProxy
+from widget.view import interface
+
+import time
+
+from widget.view.form_gtk.many2one import dialog as M2ODialog
+from modules.gui.window.win_search import win_search
+
+
+class parser_graph(interface.parser_interface):
+	def parse(self, model, root_node, fields):
+		on_write = '' #attrs.get('on_write', '')
+		view = gtk.Image()
+		view.set_from_stock('gtk-cancel', gtk.ICON_SIZE_BUTTON)
+
+		view.show()
+		return view, {}, [], on_write
+
+# vim:noexpandtab:
