59ff3e3e14a9ecff9b25455a1314dd2d2d161017
ced <>
2007-02-04 14:17:18 +0000

    CLIENT: Fix attrs for netrpc

--- bin/tools/__init__.py
+++ bin/tools/__init__.py
@@ -53,7 +53,7 @@ def node_attributes(node):
 	if attrs is None:
 		return {}
 	for i in range(attrs.length):
-		result[attrs.item(i).localName] = attrs.item(i).nodeValue
+		result[attrs.item(i).localName] = str(attrs.item(i).nodeValue)
 		if attrs.item(i).localName == "digits" and isinstance(attrs.item(i).nodeValue, (str, unicode)):
 			result[attrs.item(i).localName] = eval(attrs.item(i).nodeValue)
 	return result
