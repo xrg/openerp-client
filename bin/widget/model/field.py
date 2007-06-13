2dd6bcf6a76a357e84ed35fb8e05094444f7501f
ced <>
2007-06-13 13:15:27 +0000

    CLIENT: fix digits from forum

--- bin/widget/model/field.py
+++ bin/widget/model/field.py
@@ -131,7 +131,7 @@ class FloatField(CharField):
 	def set_client(self, model, value, test_state=True):
 		internal = model.value[self.name]
 		self.set(model, value, test_state)
-		if abs(float(internal or 0.0) - float(model.value[self.name] or 0.0)) >= (10.0**(-self.attrs.get('digits', (12,4))[1])):
+		if abs(float(internal or 0.0) - float(model.value[self.name] or 0.0)) >= (10.0**(-int(self.attrs.get('digits', (12,4))[1]))):
 			if not self.attrs.get('readonly', False):
 				model.modified = True
 				self.sig_changed(model)
