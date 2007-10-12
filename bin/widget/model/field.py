e64d2094cf82436a4f6bca0f71a932abeb46a98a
ced <>
2007-10-12 13:17:39 +0000

    Improve test on readonly field state for onchange call

--- bin/widget/model/field.py
+++ bin/widget/model/field.py
@@ -56,7 +56,7 @@ class CharField(object):
 		self.default_attrs = {}
 
 	def sig_changed(self, model):
-		if self.get_state_attrs(model)['readonly']:
+		if self.get_state_attrs(model).get('readonly', False):
 			return
 		if self.attrs.get('on_change',False):
 			model.on_change(self.attrs['on_change'])
