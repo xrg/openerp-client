2a0626889e64718ac6c85768d6ac37390abb76b2
ced <>
2007-10-19 12:15:14 +0000

    Better coding

--- bin/widget/view/form_gtk/spinint.py
+++ bin/widget/view/form_gtk/spinint.py
@@ -35,9 +35,10 @@ import interface
 class spinint(interface.widget_interface):
 
 	def __init__(self, window, parent, model, attrs={}):
+		interface.widget_interface.__init__(self, window, parent, model, attrs)
+
 		adj = gtk.Adjustment(0.0, -sys.maxint, sys.maxint, 1.0, 5.0, 5.0)
 		self.widget = gtk.SpinButton(adj, 1, digits=0)
-		interface.widget_interface.__init__(self, window, parent, model, attrs)
 		self.widget.set_numeric(True)
 		self.widget.set_width_chars(5)
 		self.widget.set_activates_default(True)
