2ba15ccc322a1f5f1eb2d570df640fc553b96378
ced <>
2007-11-26 15:45:50 +0000

    Fix boolean textual value on tree view

--- bin/widget/view/tree_gtk/parser.py
+++ bin/widget/view/tree_gtk/parser.py
@@ -215,6 +215,9 @@ class Boolean(Int):
 		self.renderer = gtk.CellRendererToggle()
 		self.renderer.connect('toggled', self._sig_toggled)
 
+	def get_textual_value(self, model):
+		return model[self.field_name].get_client(model) or 0
+
 	def setter(self, column, cell, store, iter):
 		model = store.get_value(iter, 0)
 		value = self.get_textual_value(model)
