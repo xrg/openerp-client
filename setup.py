b648d16888d4612fb284d0c1251b6cf8b5f14ea1
knipknap <knipknap@3d0d11d8-4750-0410-97fd-29bed8d48615>
2008-06-22 21:34:32 +0000

    Initial import.
    git-svn-id: https://spiff-gtkwidgets.googlecode.com/svn/trunk@2 3d0d11d8-4750-0410-97fd-29bed8d48615

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from os.path    import dirname, join
+srcdir = join(dirname(__file__), 'src')
+setup(name             = 'Spiff Gtk Widgets',
+      version          = '1.9.0',
+      description      = 'A collection of useful Gtk widgets',
+      long_description = \
+"""
+Spiff Gtk Widgets contains a number of Gtk widgets that are used 
+by other Spiff components.
+""",
+      author           = 'Samuel Abels',
+      author_email     = 'cheeseshop.python.org@debain.org',
+      license          = 'GPLv2',
+      package_dir      = {'': srcdir},
+      packages         = [p for p in find_packages(srcdir)],
+      install_requires = ['pygtk'],
+      keywords         = 'spiff gtk widgets textview annotations',
+      url              = 'http://code.google.com/p/spiff/',
+      classifiers      = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+      ])
---
+++ src/AnnotatedTextView/__init__.py
@@ -0,0 +1,5 @@
+from AnnotatedTextView import AnnotatedTextView
+
+import inspect 
+__all__ = [name for name, obj in locals().items()
+           if not (name.startswith('_') or inspect.ismodule(obj))]
