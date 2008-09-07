8954032ca9b0a205b20df3c8eb5dea3280ccd2aa
Christophe Simonis <chs@tinyerp.com>
2008-09-07 22:51:42 +0200

    bugfix wih calendar widget

--- bin/widget/screen/screen.py
+++ bin/widget/screen/screen.py
@@ -477,13 +477,9 @@ class Screen(signal_event.signal_event):
         if res_id:
             self.current_model = self.models[res_id]
         if self.views:
-            #XXX To remove when calendar will be implemented
-            #if self.current_view.view_type == 'calendar' and \
-            #        len(self.views) > 1:
-            #    self.switch_view()
             self.current_view.display()
             self.current_view.widget.set_sensitive(bool(self.models.models or (self.current_view.view_type!='form') or self.current_model))
-            self.search_active(self.current_view.view_type in ('tree', 'graph'))
+            self.search_active(self.current_view.view_type in ('tree', 'graph', 'calendar'))
 
     def display_next(self):
         self.current_view.set_value()
