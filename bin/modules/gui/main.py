b3a84b5558499e65a58399a3a5900bab53d03681
Stephane Wirtel <stephane@tinyerp.com>
2008-10-26 16:00:38 +0100

    Add a comment to specify the version since you can use this function (taken from reference manual of pygtk)

--- bin/modules/gui/main.py
+++ bin/modules/gui/main.py
@@ -932,6 +932,7 @@ class terp_main(service.Service):
         box.pack_end(closebtn, False, False)
         self.notebook.append_page(win.widget, box)
         if hasattr(self.notebook, 'set_tab_reorderable' ):
+            # since pygtk 2.10
             self.notebook.set_tab_reorderable(win.widget, True)
 
         closebtn.connect("clicked", self.sig_win_close,win.widget)
