2dfa6e8b528521b28e058cc174ed137c68ad582b
Fabien Pinckaers <fp@tinyerp.com>
2008-12-09 09:53:21 +0100

    pager

--- bin/widget/view/screen_container.py
+++ bin/widget/view/screen_container.py
@@ -39,28 +39,37 @@ class screen_container(object):
     def widget_get(self):
         return self.vbox
 
-    def add_filter(self, widget, fnct, clear_fnct):
+    def add_filter(self, widget, fnct, clear_fnct, next_fnct, prev_fnct):
         self.filter_vbox = gtk.VBox(spacing=1)
         self.filter_vbox.set_border_width(1)
-        #label = gtk.Label(_('Search'))
-        #label.set_alignment(0.0, 0.5)
-        #label.show()
-        #self.filter_vbox.pack_start(label, expand=True, fill=False)
+
         hs = gtk.HSeparator()
         hs.show()
         self.filter_vbox.pack_start(hs, expand=True, fill=False)
         self.filter_vbox.pack_start(widget, expand=True, fill=True)
+
         hb = gtk.HButtonBox()
         hb.set_spacing(5)
         hb.set_layout(gtk.BUTTONBOX_END)
+
+        self.but_previous = gtk.Button(stock=gtk.STOCK_GO_BACK)
+        self.but_previous.connect('clicked', prev_fnct)
+        self.but_next = gtk.Button(stock=gtk.STOCK_GO_FORWARD)
+        self.but_next.connect('clicked', next_fnct)
+        hb.pack_start(self.but_previous, expand=False, fill=False)
+        hb.pack_start(self.but_next, expand=False, fill=False)
+
         button_clear = gtk.Button(stock=gtk.STOCK_CLEAR)
         button_clear.connect('clicked', clear_fnct)
         hb.pack_start(button_clear, expand=False, fill=False)
+
         self.button = gtk.Button(stock=gtk.STOCK_FIND)
         self.button.connect('clicked', fnct)
         self.button.set_property('can_default', True)
+
         hb.pack_start(self.button, expand=False, fill=False)
         hb.show_all()
+
         self.filter_vbox.pack_start(hb, expand=False, fill=False)
         hs = gtk.HSeparator()
         hs.show()
