3a143de761d62d37c6988208eb5fe283f5d45db5
Stephane Wirtel <stephane@tinyerp.com>
2008-12-22 21:02:06 +0100

    [IMP] OpenERP 5.0 RC2

--- bin/release.py
+++ bin/release.py
@@ -22,7 +22,7 @@
 ##############################################################################
 
 name = 'openerp-client'
-version = '5.0.0_rc1'
+version = '5.0.0_rc2'
 description = 'OpenERP Client'
 long_desc = '''\
 OpenERP is a complete ERP and CRM. The main features are accounting (analytic
