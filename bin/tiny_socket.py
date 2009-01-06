04d812a590005bf2fa50abed4f3009fadb59a9bf
Stephane Wirtel <stephane@tinyerp.com>
2009-01-06 22:47:00 +0100

    [REF] Clean the netrpc protocol

--- bin/tiny_socket.py
+++ bin/tiny_socket.py
@@ -39,6 +39,7 @@ class mysocket:
         else:
             self.sock = sock
         self.sock.settimeout(120)
+
     def connect(self, host, port=False):
         if not port:
             protocol, buf = host.split('//')
@@ -47,40 +48,33 @@ class mysocket:
             host = DNS_CACHE[host]
         self.sock.connect((host, int(port)))
         DNS_CACHE[host], port = self.sock.getpeername()
+
     def disconnect(self):
         self.sock.shutdown(socket.SHUT_RDWR)
         self.sock.close()
+
     def mysend(self, msg, exception=False, traceback=None):
         msg = cPickle.dumps([msg,traceback])
         size = len(msg)
         self.sock.send('%8d' % size)
         self.sock.send(exception and "1" or "0")
-        totalsent = 0
-        while totalsent < size:
-            sent = self.sock.send(msg[totalsent:])
-            if sent == 0:
-                raise RuntimeError, "socket connection broken"
-            totalsent = totalsent + sent
+        self.sock.sendall(msg)
+
     def myreceive(self):
-        buf=''
-        while len(buf) < 8:
-            chunk = self.sock.recv(8 - len(buf))
-            if chunk == '':
-                raise RuntimeError, "socket connection broken"
-            buf += chunk
-        size = int(buf)
-        buf = self.sock.recv(1)
-        if buf != "0":
-            exception = buf
-        else:
-            exception = False
-        msg = ''
-        while len(msg) < size:
-            chunk = self.sock.recv(size-len(msg))
-            if chunk == '':
-                raise RuntimeError, "socket connection broken"
-            msg = msg + chunk
-        res = cPickle.loads(msg)
+        def read(socket, size):
+            buf=''
+            while len(buf) < size:
+                chunk = self.sock.recv(size - len(buf))
+                if chunk == '':
+                    raise RuntimeError, "socket connection broken"
+                buf += chunk
+            return buf
+
+        size = int(read(self.sock, 8))
+        buf = read(self.sock, 1)
+        exception = buf != '0' and buf or False
+        res = cPickle.loads(read(self.sock, size))
+
         if isinstance(res[0],Exception):
             if exception:
                 raise Myexception(str(res[0]), str(res[1]))
