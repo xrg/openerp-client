a128d5f8e687350486e9a92bebaac02fe3bdb512
Christophe Simonis <christophe@taupe>
2009-02-05 00:48:42 +0100

    [FIX] net-rpc now works on macosx

--- bin/tiny_socket.py
+++ bin/tiny_socket.py
@@ -21,7 +21,7 @@
 ##############################################################################
 import socket
 import cPickle
-import marshal
+import sys
 
 DNS_CACHE = {}
 
@@ -50,7 +50,10 @@ class mysocket:
         DNS_CACHE[host], port = self.sock.getpeername()
 
     def disconnect(self):
-        self.sock.shutdown(socket.SHUT_RDWR)
+        # on Mac, the connection is automatically shutdown when the server disconnect.
+        # see http://bugs.python.org/issue4397
+        if sys.platform != 'darwin':
+            self.sock.shutdown(socket.SHUT_RDWR)
         self.sock.close()
 
     def mysend(self, msg, exception=False, traceback=None):
