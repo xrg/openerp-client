26a1898bb6686901458fb3aabc5d29303e636dfc
knipknap <knipknap@3d0d11d8-4750-0410-97fd-29bed8d48615>
2009-04-30 19:02:21 +0000

    Add a option box element.
    git-svn-id: https://spiff-gtkwidgets.googlecode.com/svn/trunk@55 3d0d11d8-4750-0410-97fd-29bed8d48615

---
+++ src/SpiffGtkWidgets/FormEditor/Elements/OptionBox.py
@@ -0,0 +1,44 @@
+# Copyright (C) 2009 Samuel Abels <http://debain.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+import gobject, gtk
+from gtk import gdk
+from Element import Element
+
+class OptionBox(Element):
+    name     = 'optionmenu'
+    caption  = 'Option Box'
+    xoptions = gtk.EXPAND|gtk.FILL
+    yoptions = gtk.FILL
+
+    def __init__(self, options = None):
+        Element.__init__(self, gtk.ComboBox())
+        self.options = []
+        for option in options or []:
+            self.add_option(option)
+
+
+    def copy(self):
+        return OptionBox(self.options)
+
+
+    def add_option(self, option):
+        self.options.append(option)
+        self.child.append_text(gtk.MenuItem(option))
+
+
+    def get_options(self):
+        return self.options
+
+gobject.type_register(OptionBox)
--- src/SpiffGtkWidgets/FormEditor/Elements/__init__.py
+++ src/SpiffGtkWidgets/FormEditor/Elements/__init__.py
@@ -1,9 +1,10 @@
-from EntryBox import EntryBox
-from Button   import Button
-from Label    import Label
-from Table    import Table
-from Target   import Target
-from TextBox  import TextBox
+from EntryBox  import EntryBox
+from Button    import Button
+from Label     import Label
+from OptionBox import OptionBox
+from Table     import Table
+from Target    import Target
+from TextBox   import TextBox
 
 import inspect 
 __all__ = [name for name, obj in locals().items()
