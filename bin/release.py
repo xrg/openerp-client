ca791737dff7e0af331d43d3598d338eba25ec8d
ame (Tiny) <ame@tinyerp.com>
2009-09-22 18:36:45 +0530

    [REL] 5.0.6

--- bin/release.py
+++ bin/release.py
@@ -22,7 +22,7 @@
 ##############################################################################
 
 name = 'openerp-client'
-version = '5.0.5'
+version = '5.0.6'
 description = 'OpenERP Client'
 long_desc = '''\
 OpenERP is a complete ERP and CRM. The main features are accounting (analytic
