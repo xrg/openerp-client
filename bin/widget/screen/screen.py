5ca52cc69dcc4e33c25e85c51516c9dc1b710207
Jay (Open ERP) <jvo@tinyerp.com>
2010-01-12 13:04:19 +0530

    [FIX] Context when contains default_field,can pass id instead of [ids]

--- bin/widget/screen/screen.py
+++ bin/widget/screen/screen.py
@@ -550,6 +550,8 @@ class Screen(signal_event.signal_event):
         return id
 
     def load(self, ids):
+        if not isinstance(ids,list):
+            ids = [ids]
         self.models.load(ids, display=False)
         self.current_view.reset()
         if ids:
