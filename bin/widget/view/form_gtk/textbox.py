8d15b643af01dc3de6558ee3fcf14067c49bb3e7
olt@tinyerp.com <>
2010-02-01 16:23:09 +0100

    [IMP] text widget: added an option ('client.form_text_spellcheck') to have 'spell check' active or not

--- bin/widget/view/form_gtk/textbox.py
+++ bin/widget/view/form_gtk/textbox.py
@@ -23,6 +23,9 @@ import gtk
 
 import interface
 import locale
+import options
+
+
 class textbox(interface.widget_interface):
     def __init__(self, window, parent, model, attrs={}):
         interface.widget_interface.__init__(self, window, parent, model, attrs)
@@ -38,12 +41,13 @@ class textbox(interface.widget_interface):
         self.tv.set_accepts_tab(False)
         self.tv.connect('focus-out-event', lambda x,y: self._focus_out())
         if not attrs.get('readonly'):
-            try:
-                import gtkspell
-                gtkspell.Spell(self.tv).set_language(locale.getlocale()[0])
-            except:
-                # No word list may not be found for the language
-                pass
+            if options.options['client.form_text_spellcheck']:
+                try:
+                    import gtkspell
+                    gtkspell.Spell(self.tv).set_language(locale.getlocale()[0])
+                except:
+                    # No word list may not be found for the language
+                    pass
         self.widget.add(self.tv)
         self.widget.show_all()
 
