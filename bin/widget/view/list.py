74bc17c05db1b3bd84d513cfdc79cec7aaea0595
nch@tinyerp.com <>
2010-02-08 14:10:33 +0530

    [FIX]:editable list with boolean field

--- bin/widget/view/list.py
+++ bin/widget/view/list.py
@@ -593,16 +593,17 @@ class ViewList(parser_view):
         self.widget_tree.editable = value
         for col in self.widget_tree.get_columns():
             for renderer in col.get_cell_renderers():
+                write_enable = value
+                if isinstance(renderer, gtk.CellRendererToggle):
+                    renderer.set_property('activatable', write_enable)
+                elif not isinstance(renderer, gtk.CellRendererProgress) and not isinstance(renderer, gtk.CellRendererPixbuf):
+                    renderer.set_property('editable', value)
                 if value in ('top','bottom'):
-                    write_enable = value
-                    if isinstance(renderer, gtk.CellRendererToggle):
-                        renderer.set_property('activatable', write_enable)
-                    elif isinstance(renderer, (gtk.CellRendererText, gtk.CellRendererCombo, date_renderer.DecoratorRenderer)):
+                    if isinstance(renderer, (gtk.CellRendererText, gtk.CellRendererCombo, date_renderer.DecoratorRenderer)):
                         renderer.set_property('editable', write_enable)
                     if write_enable:
                         renderer.connect_after('editing-started', send_keys, self.widget_tree)
-                elif not isinstance(renderer, gtk.CellRendererProgress) and not isinstance(renderer, gtk.CellRendererPixbuf):
-                    renderer.set_property('editable', value)
+
 
     def set_invisible_attr(self):
         for col in self.widget_tree.get_columns():
