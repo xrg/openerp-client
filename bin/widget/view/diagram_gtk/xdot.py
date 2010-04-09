68def991c2fa824eb7f6650f995b2fccf750a62b
HDA (OpenERP) <hda@tinyerp.com>
2010-04-09 19:16:55 +0530

    Added licence in xdot

--- bin/widget/view/diagram_gtk/xdot.py
+++ bin/widget/view/diagram_gtk/xdot.py
@@ -1,26 +1,31 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
+#!/usr/bin/env python
 #
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+# Copyright 2008 Jose Fonseca
+# OpenERP, Open Source Management Solution
+# Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
-__version__ = "0.4"
+'''Visualize dot graphs via the xdot format.'''
 
+__author__ = "Jose Fonseca"
+
+__version__ = "0.4"
 
 import os
 import sys
