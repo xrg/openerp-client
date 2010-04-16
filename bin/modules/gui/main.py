24e44c56cd6b223f9ff1e9e132df77feb22d02e8
Chris Halls <chris.halls@credativ.co.uk>
2010-04-16 14:47:36 +0100

    Show database name in window title
    This makes it easier to work with windows open on several databases at once.

--- bin/modules/gui/main.py
+++ bin/modules/gui/main.py
@@ -1104,6 +1104,7 @@ class terp_main(service.Service):
         self.glade.get_widget('user').set_sensitive(True)
         self.glade.get_widget('form').set_sensitive(True)
         self.glade.get_widget('plugins').set_sensitive(True)
+        self.window.set_title(_('%s - OpenERP') % res[5] )
         return True
 
     def sig_logout(self, widget):
@@ -1132,6 +1133,7 @@ class terp_main(service.Service):
         self.glade.get_widget('user').set_sensitive(False)
         self.glade.get_widget('form').set_sensitive(False)
         self.glade.get_widget('plugins').set_sensitive(False)
+        self.window.set_title(_('OpenERP') )
         rpc.session.logout()
         return True
 
