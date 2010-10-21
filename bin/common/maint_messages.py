fdc5f768f9cb1ac9900d41141c1444d41b6d29eb
P. Christeas <p_christ@hol.gr>
2010-10-21 19:41:59 +0300

    common: separate the maintenance contract strings into own file
    These large chunk of texts made common.py hard to read.

---
+++ bin/common/maint_messages.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2010 OpenERP SA. (<http://www.openerp.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import gettext
+
+message_no_contract = _("""
+<b>An unknown error has been reported.</b>
+
+<b>You do not have a valid OpenERP maintenance contract !</b>
+If you are using OpenERP in production, it is highly suggested to subscribe
+a maintenance program.
+
+The OpenERP maintenance contract provides you a bugfix guarantee and an
+automatic migration system so that we can fix your problems within a few
+hours. If you had a maintenance contract, this error would have been sent
+to the quality team of the OpenERP editor.
+
+The maintenance program offers you:
+* Automatic migrations on new versions,
+* A bugfix guarantee,
+* Monthly announces of potential bugs and their fixes,
+* Security alerts by email and automatic migration,
+* Access to the customer portal.
+
+You can use the link bellow for more information. The detail of the error
+is displayed on the second tab.
+""")
+
+message_partial_contract = _("""
+<b>An unknown error has been reported.</b>
+
+Your maintenance contract does not cover all modules installed in your system !
+If you are using OpenERP in production, it is highly suggested to upgrade your
+contract.
+
+If you have developed your own modules or installed third party module, we
+can provide you an additional maintenance contract for these modules. After
+having reviewed your modules, our quality team will ensure they will migrate
+automatically for all future stable versions of OpenERP at no extra cost.
+
+Here is the list of modules not covered by your maintenance contract:
+%s
+
+You can use the link bellow for more information. The detail of the error
+is displayed on the second tab.""")
+
+#eof
