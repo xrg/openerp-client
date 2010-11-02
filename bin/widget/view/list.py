eab73dccdc902aa3280e1e7ca7e2089e025a0ac6
nch@tinyerp.com <>
2010-11-02 17:00:56 +0530

    [FIX]:cannot marshal None unless allow_none is enabled

--- bin/widget/view/list.py
+++ bin/widget/view/list.py
@@ -123,7 +123,7 @@ class list_record(object):
             return
         self.loaded = True
         gb = self.context.get('group_by', [])
-        no_leaf = self.context.get('group_by_no_leaf')
+        no_leaf = self.context.get('group_by_no_leaf', False)
         if gb or no_leaf:
             records = rpc.session.rpc_exec_auth('/object', 'execute', self.mgroup.resource, 'read_group',
                 self.context.get('__domain', []) + (self.domain or []), self.mgroup.fields.keys(), gb, 0, False, self.context)
