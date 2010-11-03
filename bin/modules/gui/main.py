a83b113ea1a25015995aa8bb099132133afe2658
nch@tinyerp.com <>
2010-11-03 16:56:34 +0530

    [IMP]:connection protocol renamed choices

--- bin/modules/gui/main.py
+++ bin/modules/gui/main.py
@@ -412,8 +412,8 @@ def _server_ask(server_widget, parent=None):
     protocol_widget = win_gl.get_widget('protocol')
 
     protocol = {
-        'XML-RPC': 'http://',
-        'NET-RPC (faster)': 'socket://',
+        'XML-RPC (port : 8069)': 'http://',
+        'NET-RPC (faster)(port : 8070)': 'socket://',
     }
 
     if check_ssl():
