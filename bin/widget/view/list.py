8d5c84f7fbe60ffa1f2b3ccc9f31f9a6286cdb98
nch@tinyerp.com <>
2010-11-03 18:02:45 +0530

    [FIX]:group by on boolean field should show False value as unticked

--- bin/widget/view/list.py
+++ bin/widget/view/list.py
@@ -144,7 +144,7 @@ class list_record(object):
                     for field in gb:
                         if not rec.value.get(field, False):
                             field_type = self.mgroup.fields.get(field, {}).get('type', False)
-                            if field in inner_gb or field_type in ('integer', 'float'):
+                            if field in inner_gb or field_type in ('integer', 'float', 'boolean'):
                                 continue
                             rec.value[field] = 'Undefined'
                             rec.field_with_empty_labels.append(field)
