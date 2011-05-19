e705a371fa21d7792a24d095987a9d84918c7b0c
Yogesh (OpenERP) <ysa@tinyerp.com>
2011-05-19 16:35:12 +0530

    [FIX] Get default value of model and eval with context and context pass in screen on one2many field.

--- bin/widget/view/form_gtk/one2many_list.py
+++ bin/widget/view/form_gtk/one2many_list.py
@@ -33,6 +33,7 @@ from widget.screen import Screen
 from pager import pager
 import service
 import tools
+import rpc
 
 
 class dialog(object):
@@ -177,13 +178,16 @@ class one2many_list(interface.widget_interface):
         # 'default_get' attribute for the same effect (pending removal)
         default_get_ctx = (attrs.get('default_get') or attrs.get('context'))
 
+        fields = rpc.session.rpc_exec_auth('/object', 'execute', model, 'fields_get', False, self.context)
+        res = rpc.session.rpc_exec_auth_try('/object', 'execute', model, 'default_get', fields)
+        self.context = tools.expr_eval("dict(%s)" % attrs.get('context',"{}"), res)
         self.screen = Screen(attrs['relation'],
                             view_type=attrs.get('mode','tree,form').split(','),
                             parent=self.parent, views_preload=attrs.get('views', {}),
                             tree_saves=attrs.get('saves', False),
                             create_new=True,
                             row_activate=self._on_activate,
-                            default_get=default_get_ctx,
+                            default_get=default_get_ctx, context= self.context,
                             window=self._window, readonly=self._readonly, limit=pager.DEFAULT_LIMIT)
 
         self.screen.type = 'one2many'
