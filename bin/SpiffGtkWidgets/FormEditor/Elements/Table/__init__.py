8fe48c0450e9968aeb6d0e8d971c1d768cfc5797
P. Christeas <xrg@openerp.com>
2011-06-09 13:53:55 +0300

    SpiffGtkWidgets: remove
    We have to use the upstream project (Fedora OS policy), remove our
    custom branch. Upstream has our patches.

--- bin/SpiffGtkWidgets/FormEditor/Elements/Table/__init__.py
+++
@@ -1,6 +0,0 @@
-from Table       import Table
-from Preferences import Preferences
-
-import inspect 
-__all__ = [name for name, obj in locals().items()
-           if not (name.startswith('_') or inspect.ismodule(obj))]
--- bin/SpiffGtkWidgets/Toolbox/__init__.py
+++
@@ -1,6 +0,0 @@
-from Toolbox   import Toolbox
-from ToolGroup import ToolGroup
-
-import inspect 
-__all__ = [name for name, obj in locals().items()
-           if not (name.startswith('_') or inspect.ismodule(obj))]
