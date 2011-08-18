1741d015f8bf957b27cb3088eab514e82c24d3e4
Stephane Wirtel <stw@openerp.com>
2011-08-18 13:10:50 +0200

    [RELEASE] OpenERP 6.0.3

--- bin/release.py
+++ bin/release.py
@@ -20,7 +20,7 @@
 #
 
 name = 'openerp-client'
-version = '6.0.2'
+version = '6.0.3'
 major_version = '6.0'
 description = 'OpenERP Client'
 long_desc = '''OpenERP is a complete ERP and CRM. The main features are accounting (analytic
