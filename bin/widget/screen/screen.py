d1048b810fd51a02e857dc0692d38e78d723a9e9
Naresh (OpenERP) <nch@tinyerp.com>
2012-01-24 12:55:44 +0530

    [IMP]:commit 1928

--- bin/widget/screen/screen.py
+++ bin/widget/screen/screen.py
@@ -426,7 +426,8 @@ class Screen(signal_event.signal_event):
             pass
 
     def _model_changed(self, model_group, model):
-        if (not model) or (model==self.current_model) and  self.current_view.view_type != 'calendar':
+        if (not model) or (model==self.current_model) and  self.current_view \
+            and self.current_view.view_type != 'calendar':
             self.display()
 
     def _get_current_model(self):
