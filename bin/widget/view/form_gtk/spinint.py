185e4b8f71c221ff4fcb34c630fd664681b0d815
Ravi Gohil (Open ERP) <rgo@tinyerp.com>
2012-10-18 18:48:56 +0530

    [FIX]: sys.maxint restricts users from entering value > 2147483647 for integer/float fields : (Maintenance Case : 580557)

--- bin/widget/view/form_gtk/spinint.py
+++ bin/widget/view/form_gtk/spinint.py
@@ -30,7 +30,7 @@ class spinint(interface.widget_interface):
     def __init__(self, window, parent, model, attrs={}):
         interface.widget_interface.__init__(self, window, parent, model, attrs)
 
-        adj = gtk.Adjustment(0.0, -sys.maxint, sys.maxint, 1.0, 5.0)
+        adj = gtk.Adjustment(0.0, -2**100, 2**100, 1.0, 5.0)
         self.widget = gtk.SpinButton(adj, 1, digits=0)
         self.widget.set_width_chars(5)
         self.widget.set_activates_default(True)
